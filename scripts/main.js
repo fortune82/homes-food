5161b188dadde1d2a5c59c1375679444aaef3dac
fortuna8.2 <fortuna8228@gmail.com>
2020-12-13 00:30:01 +0200

    сделал слайдер и ингредиенты

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,38 @@ miniMenu.addEventListener("click", () => {
     menu.classList.toggle('show');
 })
 
+// ------------при нажатии на ссылку (пункт меню) закрывается меню и бургер превращается из крестика в бургер
 backMenu.addEventListener("click", () => {
     miniMenu.classList.remove('active');
     menu.classList.remove('show');
 })
+
+
+// ---------------слайдер ---------------------------------
+$(document).ready(function () {
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        infinite: true,
+        slideToShow: 3,
+        slideToScroll: 1,
+        dots: false, // убираем точки
+        arrows: false, // убираем стрелки
+        centerMode: true, // центрируем слайд (картинку)
+        touchMove: true, // чтоб пальцем перемещать слайды
+        variableWidth: true, // это надо для того, чтоб выставлять свою ширину картинок
+        // responsive: [{
+        //         breakpoint: 768,
+        //         settings: {
+        //             slidesToShow: 2,
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //         }
+        //     }
+        // ]
+    });
+});
